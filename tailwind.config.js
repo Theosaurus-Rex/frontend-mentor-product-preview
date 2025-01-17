50b8bf5e986b051fcb21211f1aea86468591106e
Theosaurus-Rex <theosaurus13@gmail.com>
2025-01-17 15:11:34 +1100

    chore: Configure color theme extension in tailwind with project palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ module.exports = {
         montserrat: ["Montserrat", "sans-serif"],
         fraunces: ["Fraunces", "serif"],
       },
+      colors: {
+        primary: {
+          cyan: "hsl(158, 36%, 37%)",
+          cream: "hsl(30, 38%, 92%)",
+        },
+        neutral: {
+          dark: "hsl(212, 21%, 14%)",
+          medium: "hsl(228, 12%, 48%)",
+          light: "hsl(0, 0%, 100%)",
+        },
+      },
     },
   },
   plugins: [],
